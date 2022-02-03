77f550337a9b40a590d12b2e23343e78a77aa058
Pedro Carvalho <pedro.carvalho2806@hotmail.com>
2022-02-03 01:12:39 -0300

    segunda parte

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
 import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -27,9 +29,28 @@ export default () => {
     loadAll();
   }, []);
 
+  useEffect(() => {
+
+    const scrollListiner = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      }else{
+        setBlackHeader(false);
+      }
+    }
+    
+    window.addEventListener('scroll', scrollListiner);
+    return () => {
+      window.addEventListener('scroll', scrollListiner);
+    }
+  }, []);
+
   // header - destaque - as listas - rodapé
   return (
     <div className="page">
+
+      <Header black={blackHeader} />
+      
       {featuredData && <FeaturedMovie item={featuredData} />}
 
       <section className="lists">
